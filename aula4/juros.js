b0cbc8cccbf52dd7540212838ad5b71a4f4f7d5d
Ênnya Gomes <ennyagomesoc@gmail.com>
2024-01-22 01:11:45 -0300

    .

---
+++ aula4/juros.js
@@ -0,0 +1,32 @@
+function aplicarDesconto(valor, desconto){
+    return valor - (valor * (desconto/100))
+
+
+}
+
+function aplicarJuros(valor, juros)
+{
+
+
+}
+
+const precoEtiqueta = 10;
+const formaPagamento = 1;
+
+
+
+if(formaPagamento == 1){
+    console.log(aplicarDesconto(precoEtiqueta, 10))
+}
+else
+if(formaPagamento == 2){
+
+}
+else
+if(formaPagamento == 3){
+
+}
+else
+if(formaPagamento == 4){
+
+}
