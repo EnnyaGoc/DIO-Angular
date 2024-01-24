dee0debc631040691f81e50867929e05a9afa927
Ênnya Gomes <ennyagomesoc@gmail.com>
2024-01-24 15:31:25 -0300

    Update aula.js

--- aula5/aula.js
+++ aula5/aula.js
@@ -1,9 +1,27 @@
-const vitor = {
-    nome: 'Vitor',
-    idade: 25
-};
-
-
-console.log(vitor.nome);
-console.log(vitor.idade);
-console.log(vitor);
+class Pessoa{
+    nome;
+    idade;
+    anodenascimento;
+  
+    constructor(nome, idade){
+      this.nome = nome;
+      this.idade = idade;
+      this.anodenascimento = 2024 - idade;
+    }
+    descrever(){
+      console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`)
+  
+    }
+  }
+  
+  const vitor = new Pessoa('vitor', 25);
+  
+  const renan = new Pessoa();
+  renan.nome = 'renan';
+  renan.idade = 30;
+  
+  
+  console.log(vitor);
+  console.log(renan);
+  
+  
