2a81127714aca12c96fd81570f5928b72010b325
Ênnya Gomes <ennyagomesoc@gmail.com>
2024-02-04 16:13:32 -0300

    .

--- POKEDEX/assets/js/main.js
+++ POKEDEX/assets/js/main.js
@@ -1,39 +1,47 @@
 
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+const limit = 5;
+let offset = 0;
 
 
 
-function convertPokemonToHtml(pokemon){
-    return  `
-    <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
 
-
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type" ${type}>${type}</li>`).join('')}
-                </ol>
-
-            <img src="${pokemon.photo}" alt="${pokemon.name}">
-
-
-            </div>
-        </li>
-    `
+function loadPokemonItens(offset, limit){
+    
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    
+        //no map eu posso passar uma funçao q transforma um elemento em outro
+        //essa funçao vai transformar o pokemon num html
+        //join junta todos os elementos da lista em uma string 
+        //incrementando o item no html
+        const newHtml = pokemons.map((pokemon) => 
+        `
+        <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+    
+    
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type" ${type}>${type}</li>`).join('')}
+                    </ol>
+    
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
+    
+    
+                </div>
+            </li>
+        `).join('')
+        pokemonList.innerHTML += newHtml
+       
+    
+    }) 
 }
 
-const pokemonList = document.getElementById('pokemonList')
+loadPokemonItens(offset, limit)
 
-
-
- 
-pokeApi.getPokemons().then((pokemons = []) => {
-    
-    //no map eu posso passar uma funçao q transforma um elemento em outro
-    //essa funçao vai transformar o pokemon num html
-    //join junta todos os elementos da lista em uma string 
-    //incrementando o item no html
-    pokemonList.innerHTML += pokemons.map(convertPokemonToHtml).join('')
-   
-
-}) 
+ loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    loadPokemonItens(offset, limit)
+ })
